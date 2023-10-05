32cd272782fede6a300b4330c89b42084010623f
ronke <ronkeashaju@gmail.com>
2023-10-05 08:19:25 -0700

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,19 @@
+#!/usr/bin/python3
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
+    """
+    Prototype that determines if all the boxes can be opened.
+    """
+    size = len(boxes)  # size of the list of boxes.
+    checkbox = {}  # dictionary that will contain the boxes that can be opened.
+    index = 0  # index of the box that will be checked.
 
-    n = len(boxes)
-    visited = [False] * n  # Initialize a list to keep track of visited boxes
-    visited[0] = True  # The first box is unlocked initially
-    queue = [0]  # Initialize a queue for BFS with the first box
-
-    while queue:
-        current_box = queue.pop(0)
-
-        for key in boxes[current_box]:
-            if 0 <= key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
-    return all(visited)  # Check if all boxes have been visited
-
-
-# Example usage:
-boxes = [[1], [2], [3], []]
-print(canUnlockAll(boxes))  # Output: True
+    for keys in boxes:  # for each box in the list of boxes.
+        if len(keys) == 0 or index == 0:
+            checkbox[index] = -1
+        for key in keys:
+            if key < size and key != index:
+                checkbox[key] = key  # the box is added to the dictionary.
+        if len(checkbox) == size:
+            return True  # all the boxes can be opened.
+        index += 1
+    return False
